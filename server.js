679e65b8fb6cf55fae543a1f9e75bd0ce6bf787e
Ryan Mullowney <ryan@Ryans-MacBook-Pro.local>
2018-05-02 17:11:39 -0500

    Creates starter files

---
+++ server.js
@@ -0,0 +1,8 @@
+// Dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+
+var app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
