9425f8e916d54839ec5135506b3104891d12420e
Ryan Mullowney <ryan@Ryans-MacBook-Pro.local>
2018-05-06 18:25:08 -0500

    Adds server info to connect w/ JawsDB

--- server.js
+++ server.js
@@ -18,6 +18,28 @@ app.use(express.static("public"));
 // Routes
 require("./routes/api-routes.js")(app);
 
+// Attempting to connect db to Heroku - I think it works?
+var mysql = require('mysql');
+var connection;
+if (process.env.JAWSDB_URL) { 
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'burgers_db'
+    })
+}
+connection.connect(function(err) {
+    if (err) {
+      console.error("error connecting: " + err.stack);
+      return;
+    }
+    console.log("connected as id " + connection.threadId);
+  });
+
+
 // Syncing sequelize models and starting Express app
 db.sequelize.sync({ force: true }).then(function (){
     app.listen(PORT, function (){
