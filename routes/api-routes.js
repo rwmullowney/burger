ee04954b19c009ac90087b9502ea8ffa77c4fc2a
Ryan Mullowney <ryan@Ryans-MacBook-Pro.local>
2018-05-06 23:05:26 -0500

    Sets up basic Handlebars and sorts burgers in table depending on whether they've been eaten

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,7 +1,26 @@
 // Requiring our models
 var db = require("../models");
 
-// Routes
-module.exports = function(app){
+// Declare variables
+let burgers = db.Burger;
+let eatenBurgers = [];
+let uneatenBurgers = [];
 
+
+
+// Routes
+module.exports = function (app) {
+    app.get("/", function (req, res) {
+        burgers.findAll({}).then(function (dbBurgers) {
+            // Filter through list to determine which burgers have and haven't been eaten
+            for (let i = 0; i < dbBurgers.length; i++) {
+                if (dbBurgers[i].devoured) {
+                    eatenBurgers.push(dbBurgers[i]);
+                }
+                else { uneatenBurgers.push(dbBurgers[i]) }
+            }
+            // Render the page once the burgers have been sorted
+            res.render("index", { eatenBurgers: eatenBurgers, uneatenBurgers: uneatenBurgers });
+        });
+    });
 };
--- server.js
+++ server.js
@@ -3,13 +3,19 @@ var express = require("express");
 var bodyParser = require("body-parser");
 
 var app = express();
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3030;
 
 // Require our models for syncing
 var db = require("./models");
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+// Setting up handlebars
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main"}))
+app.set("view engine", "handlebars")
 
 // Static directory
 app.use(express.static("public"));
@@ -41,7 +47,7 @@ connection.connect(function(err) {
 
 
 // Syncing sequelize models and starting Express app
-db.sequelize.sync({ force: true }).then(function (){
+db.sequelize.sync({ /*force: true*/ }).then(function (){
     app.listen(PORT, function (){
         console.log("App listening on PORT " + PORT);
     });
